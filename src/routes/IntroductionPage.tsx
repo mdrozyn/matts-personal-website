5fe1a24a2d7760b6f04f76422d88fdc6958a2ead
mdrozyn <98671484+mdrozyn@users.noreply.github.com>
2022-09-03 02:06:03 -0400

    Added some dummy copy to the Introduction page

--- src/routes/IntroductionPage.tsx
+++ src/routes/IntroductionPage.tsx
@@ -1,8 +1,14 @@
 export function IntroductionPage() {
     return (
         <div className="p-5 text-center">
-            <h1>Bio Page</h1>
-            <p>This is where Our Bio will go!</p>
+            <h1>Introduction</h1>
+            <br></br>
+            <p>Hello World!</p>
+            <p>My name is Matt Drozynski and I am a Web Developer from Mechanicsburg Pennsylvania.</p>
+            <p>From the time I was young, I was fanscinated with technology. Watching my older cousins play video games was intersting and I wanted to learn more.</p>
+            <p>As a teenager, I developed an interest in guitar playing and music recording which eventually led me to move to Philadelphia to immerse myself in the music culture and to attend Temple University.</p>
+            <p>After graduating, I have worked as a web developer, most recently for TherapyNotes.com</p>
+            <p>Thanks for checking out my website!</p>
         </div>
     )
 }
