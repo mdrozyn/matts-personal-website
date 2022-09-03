471413602a05c7c097f1b40dd8c088e241a6475c
mdrozyn <98671484+mdrozyn@users.noreply.github.com>
2022-09-03 02:20:25 -0400

    Modified Copy

--- src/routes/IntroductionPage.tsx
+++ src/routes/IntroductionPage.tsx
@@ -7,7 +7,7 @@ export function IntroductionPage() {
             <p>My name is Matt Drozynski and I am a Web Developer from Mechanicsburg Pennsylvania.</p>
             <p>From the time I was young, I was fanscinated with technology. Watching my older cousins play video games was intersting and I wanted to learn more.</p>
             <p>As a teenager, I developed an interest in guitar playing and music recording which eventually led me to move to Philadelphia to immerse myself in the music culture and to attend Temple University.</p>
-            <p>After graduating, I have worked as a web developer, most recently for TherapyNotes.com</p>
+            <p>Since graduating, I began working in the software development industry for over 4 years.</p>
             <p>Thanks for checking out my website!</p>
         </div>
     )
