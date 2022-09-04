015a0e48f9e7457e9c170909e9dbd6d8c587e6fa
mdrozyn <98671484+mdrozyn@users.noreply.github.com>
2022-09-04 13:26:10 -0400

    Added photo viewer on click

--- src/components/AdaptivePhotoLayout/AdaptivePhotoLayout.tsx
+++ src/components/AdaptivePhotoLayout/AdaptivePhotoLayout.tsx
@@ -1,9 +1,27 @@
+import PhotoViewer from 'photoviewer';
 import { ImageManager } from '../utilities/ImageManager';
 import './AdaptivePhotoLayout.css';
-
+import '../../thirdParty/styleSheets/photoviewer.css';
 export function AdaptivePhotoLayout() {
 
     const imagePaths = ImageManager.getArtPageImages();
+    const photos: Photo[] = [];
+    let photoViewer: PhotoViewer;
+    imagePaths.forEach((path) => {
+        photos.push({
+            src: path,
+            title: "Test Title"
+        })
+    })
+
+    const onImageClick = (index: number) => {
+
+        if (photoViewer) {
+            // @ts-ignore can this be resolved with a type declaration file?
+            photoViewer.close();
+        }
+        photoViewer = new PhotoViewer(photos, { index: index });
+    }
 
     return (
         <div className="adaptive-photo-layout">
@@ -11,7 +29,7 @@ export function AdaptivePhotoLayout() {
                 {imagePaths.map((imageUrl, index) => {
                     return (
                         <li key={index}>
-                            <img className='zoom' src={imageUrl} alt="" />
+                            <img className='zoom' onClick={() => { onImageClick(index) }} src={imageUrl} alt="" />
                         </li>
                     )
                 })}
@@ -19,4 +37,9 @@ export function AdaptivePhotoLayout() {
             </ul>
         </div>
     )
+}
+
+export type Photo = {
+    src: string,
+    title: string
 }
