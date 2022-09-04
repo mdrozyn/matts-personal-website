0d86d6c9a501ea3d116933168dd123397b893505
mdrozyn <98671484+mdrozyn@users.noreply.github.com>
2022-09-04 13:45:40 -0400

    Fixed image titles, refactoring

--- src/components/AdaptivePhotoLayout/AdaptivePhotoLayout.tsx
+++ src/components/AdaptivePhotoLayout/AdaptivePhotoLayout.tsx
@@ -5,23 +5,35 @@ import '../../thirdParty/styleSheets/photoviewer.css';
 export function AdaptivePhotoLayout() {
 
     const imagePaths = ImageManager.getArtPageImages();
-    const photos: Photo[] = [];
+
+    const photos: PhotoViewerImage[] = [];
     let photoViewer: PhotoViewer;
-    imagePaths.forEach((path) => {
-        photos.push({
-            src: path,
-            title: "Test Title"
-        })
-    })
 
     const onImageClick = (index: number) => {
+        displayPhotoViewer(index);
+    }
 
+    const displayPhotoViewer = (index: number)=>{
         if (photoViewer) {
             // @ts-ignore can this be resolved with a type declaration file?
             photoViewer.close();
         }
         photoViewer = new PhotoViewer(photos, { index: index });
     }
+
+    const formatImageTitles = () => {
+        imagePaths.forEach((path) => {
+            const imageNameRegex = /[^\\/]+?(?=\.\w+$)/
+            const fileName = path.match(imageNameRegex)?.toString() ?? "";
+            console.log(fileName);
+            photos.push({
+                src: path,
+                title: fileName.substring(0, fileName.indexOf('.'))
+            })
+        })
+    }
+
+    formatImageTitles();
 
     return (
         <div className="adaptive-photo-layout">
@@ -39,7 +51,7 @@ export function AdaptivePhotoLayout() {
     )
 }
 
-export type Photo = {
+export type PhotoViewerImage = {
     src: string,
     title: string
 }
