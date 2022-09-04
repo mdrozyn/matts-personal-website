289064ec6751935556c7099977b2244e4f6f95ee
mdrozyn <98671484+mdrozyn@users.noreply.github.com>
2022-09-04 17:49:44 -0400

    More styling of the theme viewer, began implementing theming

--- src/components/AdaptivePhotoLayout/AdaptivePhotoLayout.tsx
+++ src/components/AdaptivePhotoLayout/AdaptivePhotoLayout.tsx
@@ -6,26 +6,59 @@ export function AdaptivePhotoLayout() {
 
     const imagePaths = ImageManager.getArtPageImages();
 
-    const photos: PhotoViewerImage[] = [];
     let photoViewer: PhotoViewer;
+    const photos: PhotoViewerImage[] = [];
 
     const onImageClick = (index: number) => {
         displayPhotoViewer(index);
     }
 
-    const displayPhotoViewer = (index: number)=>{
+    const displayPhotoViewer = (index: number) => {
+
         if (photoViewer) {
-            // @ts-ignore can this be resolved with a type declaration file?
-            photoViewer.close();
+            closePhotoViewer();
         }
-        photoViewer = new PhotoViewer(photos, { index: index });
+
+        const photoViewerOptions: PhotoViewer.Options = {
+            index: index,
+            draggable: false,
+            resizable: false,
+            movable: false,
+            modalHeight: window.innerHeight,
+            modalWidth: window.innerWidth - 280,
+            initModalPos: {
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 280
+            },
+            callbacks: {
+                closed: () => {
+                    document.body.style.overflow = 'visible';
+                },
+                changed: () => { } // I think this parameter should be optional?
+            },
+            headerToolbar: ['close'],
+            footerToolbar: ['zoomIn', 'zoomOut', 'prev', 'next']
+        }
+
+        photoViewer = new PhotoViewer(photos, photoViewerOptions);
+
+        window.addEventListener('resize', closePhotoViewer)
+        document.body.style.overflow = 'hidden';
+    }
+
+    const closePhotoViewer = () => {
+        // @ts-ignore can this be resolved with a type declaration file?
+        photoViewer.close();
+        document.body.style.overflow = 'visible';
     }
 
     const formatImageTitles = () => {
         imagePaths.forEach((path) => {
             const imageNameRegex = /[^\\/]+?(?=\.\w+$)/
             const fileName = path.match(imageNameRegex)?.toString() ?? "";
-            console.log(fileName);
+
             photos.push({
                 src: path,
                 title: fileName.substring(0, fileName.indexOf('.'))
