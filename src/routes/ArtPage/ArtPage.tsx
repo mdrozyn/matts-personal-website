adfcc2f58e7dbfd322511fc33cf4e71da75c761c
mdrozyn <98671484+mdrozyn@users.noreply.github.com>
2022-09-05 17:16:20 -0400

    Added new designs to the art page

--- src/routes/ArtPage/ArtPage.tsx
+++ src/routes/ArtPage/ArtPage.tsx
@@ -5,8 +5,9 @@ export function ArtPage() {
         <>
             <div className="page-wrapper p-5 text-center">
                 <h1 className="mb-3">Art</h1>
-                <p className="mb-3">All designs are avalible for purchase through my&nbsp;
-                    <a href="https://www.redbubble.com/people/aliveinecho/shop?artistUserName=aliveinecho&iaCode=u-clothing">Redbubble Shop</a>
+                <p className="mb-5">All designs are avalible for purchase through my&nbsp;
+                    <a href="https://www.redbubble.com/people/aliveinecho/explore?asc=u&page=1&sortOrder=recent">Redbubble Shop</a>
+                    .
                 </p>
 
                 <AdaptivePhotoLayout />
