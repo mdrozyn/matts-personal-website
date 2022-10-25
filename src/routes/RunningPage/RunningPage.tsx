4a008f159e722894c257bd70106f8a2e291b7077
mdrozyn <98671484+mdrozyn@users.noreply.github.com>
2022-10-25 07:34:21 -0400

    Added Fall foliage content to running page

--- src/routes/RunningPage/RunningPage.tsx
+++ src/routes/RunningPage/RunningPage.tsx
@@ -7,6 +7,13 @@ import IMG_7034 from '../../images/RunningPage/TroegsRuggedTrail5KRunforConserva
 import IMG_7031 from '../../images/RunningPage/TroegsRuggedTrail5KRunforConservation/IMG_7031.jpg'
 import IMG_5601 from '../../images/RunningPage/TroegsRuggedTrail5KRunforConservation/IMG_5601.jpg'
 import IMG_3447 from '../../images/RunningPage/TroegsRuggedTrail5KRunforConservation/IMG_3447.jpg'
+import MattAtFallFoliageClassic from '../../images/RunningPage/FallFoliageClassic2022/MattAtFallFoliageClassic.jpg'
+import IMG_7377 from '../../images/RunningPage/FallFoliageClassic2022/IMG_7377.jpg'
+import IMG_7383 from '../../images/RunningPage/FallFoliageClassic2022/IMG_7383.jpg'
+import IMG_7384 from '../../images/RunningPage/FallFoliageClassic2022/IMG_7384.jpg'
+import IMG_7386 from '../../images/RunningPage/FallFoliageClassic2022/IMG_7386.jpg'
+import IMG_7389 from '../../images/RunningPage/FallFoliageClassic2022/IMG_7389.jpg'
+import IMG_7390 from '../../images/RunningPage/FallFoliageClassic2022/IMG_7390.jpg'
 
 export function RunningPage() {
 
@@ -54,31 +61,48 @@ export function RunningPage() {
             IMG_3447
         ]
     },
-]
+    {
+        Name: "Fall Foliage Classic 2022",
+        Date: "10/8/2022",
+        Location: "Schellsburg PA",
+        Time: "25:53",
+        Position: 7,
+        TotalRunners: 31,
+        Notes: "Got first for my age group! It was a beautiful day and we ran a loop around the lake at Shawnee State Park",
+        ResultsLink: "https://runsignup.com/Race/Results/131245#resultSetId-346313;perpage:100",
+        Photos: [
+            MattAtFallFoliageClassic,
+            IMG_7377,
+            IMG_7383,
+            IMG_7384,
+            IMG_7386,
+            IMG_7389,
+            IMG_7390
+        ]
+    },
+    ]
 
     return (
-        <>
-            <div className="page-wrapper p-5 text-center">
-                <h1 className="mb-3">Running</h1>
-                <p className="mb-5">A running list of all of the races that I have participated in.
-                </p>
-                <div className="mb-5">
-                    {runs.map((run, index) => {
-                        return (<Run
-                            key={index}
-                            Name={run.Name}
-                            Date={run.Date}
-                            Location={run.Location}
-                            Time={run.Time}
-                            Position={run.Position}
-                            TotalRunners={run.TotalRunners}
-                            Notes={run.Notes}
-                            ResultsLink={run.ResultsLink}
-                            Photos={run.Photos} />);
-                    })}
-                </div>
+        <div className="page-wrapper p-5 text-center">
+            <h1 className="mb-3">Running</h1>
+            <p className="mb-5">A running list of all of the races that I have participated in.
+            </p>
+            <div className="mb-5">
+                {runs.map((run, index) => {
+                    return (<Run
+                        key={index}
+                        Name={run.Name}
+                        Date={run.Date}
+                        Location={run.Location}
+                        Time={run.Time}
+                        Position={run.Position}
+                        TotalRunners={run.TotalRunners}
+                        Notes={run.Notes}
+                        ResultsLink={run.ResultsLink}
+                        Photos={run.Photos} />);
+                })}
             </div>
-        </>
+        </div>
     )
 }
 
